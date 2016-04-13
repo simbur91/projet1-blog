a6fd22a83119246131ae13a52d3970fbef021473
sbuerman <simon.buerman@gmail.com>
2016-04-14 01:06:43 +0200

    Merge branch 'master' of https://github.com/simbur91/projet1-blog
    # Conflicts:
    #	modeles/blog/commentaires_post.php

--- controllers/admin/delete.php
+++ controllers/admin/delete.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'modeles/admin/manager.php';
-if(isset($_GET['token'])) {
     delete($_GET['id'], $_GET['table']);
 
     header('Location:dashboard.php?message=supprimer');
-}
+
